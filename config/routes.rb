af6fa7096f8b4df6d8fd68396fb5fefd271753b3
HenryGarmendia <nsaneznicoyaz@Henrys-MacBook-Pro.local>
2017-02-11 13:59:51 -0600

    Generate welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # this will set the welcome/index as the root of our application
+  root 'welcome#index'
 end
