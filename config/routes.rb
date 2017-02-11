0a1f85e648406b4afbc7ae51da28fe76411c4b40
HenryGarmendia <nsaneznicoyaz@Henrys-MacBook-Pro.local>
2017-02-11 16:52:46 -0600

    Chage root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # this will set the welcome/index as the root of our application
-  root 'welcome#index'
-
   # this will add all the resources for our application
   resources :docs
+  # this is to redirect the current login user to page
+  authenticated :user do
+  	root "docs#index", as: "authenticated_root"
+  end
+
+  # this will set the welcome/index as the root of our application
+  root 'welcome#index'
 end
